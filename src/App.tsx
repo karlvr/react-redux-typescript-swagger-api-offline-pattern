489d9a8187602f9dffb3adc886051636917a7727
Karl von Randow <karl@xk72.com>
2018-05-14 10:50:48 +1200

    Rename template example to uppercase

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './modules/index';
 
 import './App.css';
-import Example from './modules/template/containers/example';
+import Example from './modules/template/containers/Example';
 import Login from './modules/auth/containers/Login';
 import Petstore from './modules/petstore/containers/Petstore';
 import ReadyGate from './modules/offline/containers/ReadyGate';
