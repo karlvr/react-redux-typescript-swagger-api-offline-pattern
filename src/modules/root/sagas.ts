287faeb8e11a69bc70534fcaa592a0a0f4634f2b
Karl von Randow <karl@xk72.com>
2018-05-14 12:12:09 +1200

    Root saga in root module

--- src/modules/root/sagas.ts
+++ src/modules/root/sagas.ts
@@ -5,16 +5,15 @@
 import { all } from 'redux-saga/effects'
 
 /* Import module sagas */
-import authSaga from './auth/sagas'
-import apiSaga from './api/sagas'
+import authSaga from '../auth/sagas'
+import apiSaga from '../api/sagas'
+import platformSaga from '../platform/sagas'
 
 /** The root saga that starts all of the other sagas. */
 export default function* rootSaga() {
 	yield all([
-		/* The API saga */
 		apiSaga(),
-
-		/* The authentication saga */
 		authSaga(),
+		platformSaga(),
 	])
 }
